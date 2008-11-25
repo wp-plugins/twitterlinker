8c1f98c46379e86dc4c3956cf3d7e14bd0eb90a1
g4m8i7 <g4m8i7@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-25 21:59:55 +0000

    Adding First Version of my Plugin
    git-svn-id: https://plugins.svn.wordpress.org/twitterlinker/trunk@75885 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Readme.txt.txt
@@ -0,0 +1,11 @@
+Install this to automagically linkify your twitterlike @name in your blog!
+
+Future fixes:
+linkify in searches
+linkify in comments
+
+Suggestions or questions?
+
+Email: tc@thursdayschild.net
+
+AIM: g4m8i7 86
---
+++ twitterlinker.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: Twitterlinker
+Plugin URI: tbd
+Description: Automagically turns an @name twitter reference into a twitter link to that person's twitter page.
+Version: 0.1
+Author: Thursday's Child
+Author URI: http://www.thrusdays-child.net
+*/
+?>
+
+<?php add_filter('the_content', 'twitterlinker'); ?>
+
+<?php add_filter('the_search_query', 'twitterlinker'); ?>
+
+<?php
+function twitterlinker($content){
+    $content = preg_replace("/\@([0-9a-zA-Z]+)/", "<a href=\"http://twitter.com/\\1\">\\0</a>", $content);
+    return $content;
+}
+// end function
+?>
