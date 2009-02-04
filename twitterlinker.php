8ea59cf76fa849bb4a95b047d92c76b3a057eee7
g4m8i7 <g4m8i7@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-04 15:21:43 +0000

    Fixed where it added new lines to the beginning of the XML. RSS Feeds will validate now.
    git-svn-id: https://plugins.svn.wordpress.org/twitterlinker/trunk@91858 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- twitterlinker.php
+++ twitterlinker.php
@@ -3,16 +3,13 @@
 Plugin Name: Twitterlinker
 Plugin URI: tbd
 Description: Automagically turns an @name twitter reference into a twitter link to that person's twitter page.
-Version: 0.1
+Version: 0.2
 Author: Thursday's Child
 Author URI: http://www.thursdays-child.net
 */
 ?>
-
 <?php add_filter('the_content', 'twitterlinker'); ?>
-
 <?php add_filter('the_search_query', 'twitterlinker'); ?>
-
 <?php
 function twitterlinker($content){
     $content = preg_replace("/\@([0-9a-zA-Z]+)/", "<a href=\"http://twitter.com/\\1\">\\0</a>", $content);
